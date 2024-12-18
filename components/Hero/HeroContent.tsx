1e07426ea05ae3721c7cd4b0d818daddb9796e97
Manas Ranjan Adha <manas865873@gmail.com>
2024-12-18 16:37:23 +0530

    add animation to Hero section

--- components/Hero/HeroContent.tsx
+++ components/Hero/HeroContent.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { motion } from "motion/react";
+
 
 const HeroContent = () => {
   return (
-    <div className="text-center max-w-6xl mx-auto">
+    <motion.div 
+    initial={{ opacity: 0, y: -10, scale: 0.5 }}
+    animate={{ opacity: 1, y: 0 , scale: 1 }}
+    transition={{ duration: 1.5, ease: "easeIn" , delay: 0.5, staggerChildren: 0.5}}
+    className="text-center max-w-6xl mx-auto">
       <h1 className="text-[#EB0000] font-[family-name:var(--font-orbitron)] text-[34px] md:text-[85px] font-black mb-4 leading-120">
         Track Your Fitness Journey
       </h1>
@@ -9,7 +16,7 @@ const HeroContent = () => {
         Discover the ultimate fitness companion with GymFluencer. Effortlessly log your workouts, count
         reps, and track calories burned. Stay motivated and achieve your goals with ease.
       </p>
-    </div>
+    </motion.div>
   );
 };
 
--- components/Hero/TrustBadge.tsx
+++ components/Hero/TrustBadge.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const TrustBadge = () => {
   return (
-    <div className="flex items-center bg-[#18181A] backdrop-blur-sm pr-4 pl-[0.6rem] py-2 rounded-full border border-[#28282C]">
-      <div className="flex -space-x-2 mr-2">
+    <motion.div 
+    initial={{ opacity: 0, y: -20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5}}
+    className="flex items-center bg-[#18181A] backdrop-blur-sm pr-4 pl-[0.6rem] py-2 rounded-full border border-[#28282C]">
+      <div 
+      className="flex -space-x-2 mr-2">
         {[1, 2, 3].map((i) => (
           <Image
             key={i}
@@ -16,7 +24,7 @@ const TrustBadge = () => {
         ))}
       </div>
       <span className="text-white font-[family-name:var(--font-space-grotesk)] font-medium leading-150">Trusted by 3+ million users</span>
-    </div>
+    </motion.div>
   );
 };
 
