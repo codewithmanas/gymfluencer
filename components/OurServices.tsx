fa2a9ffd9a031c7a1b6ce8cbc14b1990868688d8
Manas Ranjan Adha <manas865873@gmail.com>
2024-12-18 12:38:43 +0530

    add scale up in service section

--- components/OurServices.tsx
+++ components/OurServices.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const OurServices = () => {
@@ -10,57 +11,69 @@ const OurServices = () => {
             </div>
 
 
-            <div className='w-full grid grid-cols-5 mt-16 grid-rows-1'>
-                    <div className='inset-0 h-[500px] cursor-pointer flex items-center justify-center transition-transform duration-500 ease-in-out'
-                        style={{
-                            backgroundImage: "url('/assets/images/service1.png')",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
-                        <div className='text-[#DCDCDC] font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>TRACK</div>
+            <div className='w-full grid grid-cols-5 mt-16'>
+
+                    <div className='relative h-[528px] w-full overflow-hidden cursor-pointer flex items-center justify-center transition-transform duration-400 group'>
+                        <Image 
+                            src="/assets/images/service1.png" 
+                            alt="service one" 
+                            width={100} 
+                            height={100}
+                            unoptimized
+                            className="w-full h-full object-cover opacity-60 transition-transform duration-500 group-hover:scale-[1.09]"
+                        />
+                        <div className='text-[#DCDCDC] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>TRACK</div>
                     </div>
 
-                    <div className='h-[500px] cursor-pointer flex items-center justify-center'
-                        style={{
-                            backgroundImage: "url('/assets/images/asset9.png')",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
-                        <div className='text-[#DCDCDC] font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>ANALYZE</div>
+                    <div className='relative h-[528px] w-full overflow-hidden cursor-pointer flex items-center justify-center transition-transform duration-400 group'>
+                    <Image 
+                            src="/assets/images/asset9.png" 
+                            alt="service two" 
+                            width={100} 
+                            height={100}
+                            unoptimized
+                            className="w-full h-full object-cover opacity-60 transition-transform duration-500 group-hover:scale-[1.09]"
+                        />
+                        <div className='text-[#DCDCDC] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>ANALYZE</div>
                     </div>
-                    <div className='h-[500px] cursor-pointer flex items-center justify-center'
-                        style={{
-                            backgroundImage: "url('/assets/images/asset10.png')",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
-                        <div className=' text-[#DCDCDC] font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>TRAIN</div>
+
+                    <div className='relative h-[528px] w-full overflow-hidden cursor-pointer flex items-center justify-center transition-transform duration-400 group'>
+                    <Image 
+                            src="/assets/images/asset10.png" 
+                            alt="service two" 
+                            width={100} 
+                            height={100}
+                            unoptimized
+                            className="w-full h-full object-cover opacity-60 transition-transform duration-500 group-hover:scale-[1.09]"
+                        />
+
+                        <div className='text-[#DCDCDC] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>TRAIN</div>
                     </div>
-                    <div className='h-[500px] cursor-pointer flex items-center justify-center'
-                        style={{
-                            backgroundImage: "url('/assets/images/asset11.png')",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
-                        <div className='text-[#DCDCDC] font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>CONNECT</div>
+
+                    <div className='relative h-[528px] w-full overflow-hidden cursor-pointer flex items-center justify-center transition-transform duration-400 group'>
+                    <Image 
+                            src="/assets/images/asset11.png" 
+                            alt="service two" 
+                            width={100} 
+                            height={100}
+                            unoptimized
+                            className="w-full h-full object-cover opacity-60 transition-transform duration-500 group-hover:scale-[1.09]"
+                        />
+
+                        <div className='text-[#DCDCDC] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>CONNECT</div>
                     </div>
-                    <div className='h-[500px] cursor-pointer flex items-center justify-center'
-                        style={{
-                            backgroundImage: "url('/assets/images/asset12.png')",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
-                        <div className='text-[#DCDCDC] font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>CHALLENGE</div>
+
+                    <div className='relative h-[528px] w-full overflow-hidden cursor-pointer flex items-center justify-center transition-transform duration-400 group'>
+
+                    <Image 
+                            src="/assets/images/asset12.png" 
+                            alt="service two" 
+                            width={100} 
+                            height={100}
+                            unoptimized
+                            className="w-full h-full object-cover opacity-60 transition-transform duration-500 group-hover:scale-[1.09]"
+                        />
+                        <div className='text-[#DCDCDC] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[family-name:var(--font-orbitron)] text-3xl font-black uppercase'>CHALLENGE</div>
                     </div>
                     
             </div>
